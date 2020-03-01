753375ec79f88eedf69721c05c434ce6c8c3518f
Corvax <vlvete1990@yandex.ru>
2020-03-01 17:05:47 +0300

    adding sandbox

--- beginning-elm/src/Main.elm
+++ beginning-elm/src/Main.elm
@@ -1,36 +1,80 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import String exposing (..)
-{- MODEL -}
-model =
+
+-- MAIN
+
+main =
+  Browser.sandbox 
+  { 
+      init = initialModel,
+      update = update, 
+      view = view
+  }
+
+
+-- MODEL 
+type alias Model =
+  { query : String,
+    results: List { id: Int, name: String, stars: Int }
+  }
+
+-- init: Model
+initialModel =
     {
-        result = 
-            {
-                id = 1
-                , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
-                , stars = 66
-            }
+        query = "tutorial"
+        , results =
+            [
+                {
+                    id = 1
+                    , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+                    , stars = 66
+                }
+                ,{
+                    id = 2
+                    , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+                    , stars = 41
+                }
+               ,{
+                    id = 3
+                    , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+                    , stars = 35
+               }
+            ]
     }
 
-{- MAIN -}
-main = 
-    let
-        elmHubHeader = 
+elmHubHeader = 
             header []
                 [ h1 [] [ text "ElmHub" ]
                 , span [ class "tagline"] [ text "Like GitHub, but for Elm thins." ]   
                 ]
-    in
-        div [ class "content"] 
-            [ elmHubHeader
-            , ul [ class "results" ]
-                [ li []
-                    [ span [class "star-count"] [ text (fromInt model.result.stars) ],
-                    a [ href ("https://github.com" ++ model.result.name) ] 
-                    [ text model.result.name ]
-                ]
-                ]
 
+{- VIEW -}
+
+view model =
+    div [ class "content"] 
+            [ elmHubHeader
+                , ul [ class "results" ] (List.map viewSearcResult model.results)
             ]
+
+
+viewSearcResult result = 
+    li []
+        [  span [ class "start-count"] [ text (result.name)],
+         button [class "hidden-result", onClick {operation = "DELETE_BY_ID", data = result.id}] [ text "X"]]
+
+{- UPDATE -}
+
+-- update msg model 
+--    = model
+update msg model =
+    if msg.operation == "DELETE_BY_ID" then
+        { model | results = List.filter (\result -> result.id /= msg.data) model.results } 
+    else 
+        model
+
+        
