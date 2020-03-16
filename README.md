c96160047fec7ad2a13f008ce199efad11d7192c
Corvax <vlvete@inbox.ru>
2020-03-16 13:22:54 +0000

    adding LeaderBoard

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 "# elm" 
 
 elm make src/HomePage.elm --output elm.js
+
+elm-live
+elm-live src/Main.elm --open
+elm-live src/Main.elm --open -- --debug
---
+++ elm-components/src/LeaderBoard.elm
@@ -0,0 +1,61 @@
+module LeaderBoard exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+-- model
+
+type alias Model =
+    {
+        runner: List Runner
+       , query: String
+    }
+
+type alias Runner = 
+    {
+        id: Int
+       , name: String
+       , location: String
+    }
+
+initModel : Model
+initModel =
+    {
+        runner = []
+        , query = ""
+    }
+
+-- update
+
+type Msg
+    = QueryInput String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of 
+        QueryInput query ->
+            { model | query = query }
+
+-- view
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 [] [ text "Leaderboard page... So far" ]
+            , input
+                [ type_ "text" 
+                 , onInput QueryInput
+                 , value model.query
+                 , placeholder "Search for a runner..."
+                ]
+                []
+        ]
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initModel
+        , view = view
+        , update = update
+        }
