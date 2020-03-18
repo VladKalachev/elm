e05a5d199ab48baf89cc44785cd0e49f0c13a0c6
Corvax <vlvete1990@yandex.ru>
2020-03-18 20:41:39 +0300

    adding message Add and InputText

--- todo-list/src/Main.elm
+++ todo-list/src/Main.elm
@@ -2,6 +2,9 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Debug exposing (log)
 import List
 
 type alias Model =
@@ -9,8 +12,9 @@ type alias Model =
        value: String 
     }
 
-type Message 
-    = Add
+type Message = 
+    Add
+    | InputText String
 
 -- INIT
 init: Model 
@@ -20,12 +24,27 @@ init =
 -- VIEW
 view : Model -> Html Message
 view model =
-        div [] [ text model.value ] 
+        div [ class "wrapper" ] [ 
+            div [] [ text "TODO list"] 
+            , text model.value
+            ,div [] []
+            , input [ onInput InputText ] []
+            , button [ onClick Add ] [ text "add" ] 
+        ] 
     
 -- UPDATE
 update : Message -> Model -> Model
 update msg model =
-    model
+    let
+        log1 = log "msg" msg
+        log2 = log "model" model
+    in
+    
+    case msg of
+            Add ->
+                model
+            InputText inputText ->
+                { model | value = inputText }
 
 -- MAIN
 main =
